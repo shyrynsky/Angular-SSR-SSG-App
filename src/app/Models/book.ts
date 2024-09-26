60a13c7f4192b7427c5e4740073effff06924a84
Shyrynsky <shyrynsky@gmail.com>
2024-09-26 17:31:25 +0300

    made models

---
+++ src/app/Models/book.ts
@@ -0,0 +1,7 @@
+export interface Book {
+  id: number;
+  name: string;
+  author_id: number;
+  text_id: number;
+  description: string;
+}
---
+++ src/app/Models/text.ts
@@ -0,0 +1,4 @@
+export interface Text {
+  id: number;
+  value: string;
+}
